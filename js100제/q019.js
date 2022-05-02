ef1685fd850e536c01acf2ce7cd19ac72811afde
Jaenk <jaehy0429@gmail.com>
2022-05-02 23:35:43 +0900

    algorthm 03 / JS100제 스터디 Day1

---
+++ js100제/q019.js
@@ -0,0 +1,3 @@
+// 공백으로 구분하여 두 숫자 a와 b가 주어지면, a의 b승을 구하는 프로그램을 작성하세요.
+let num = prompt("숫자를 입력해주세요").split(" ")
+console.log(Math.pow(Number(num[0]),Number(num[1])))
---
+++ js100제/q027.js
@@ -0,0 +1,16 @@
+// 첫번째 입력에서는 학생의 이름이 공백으로 구분되어 입력되고, 두번째에는 그 학생의 수학 점수가 공백으로 구분되어 주어집니다.
+// 두 개를 합쳐 학생의 이름이 key이고 value가 수학 점수인 객체를 출력해주세요.
+
+// 입력
+// Yujin Hyewon
+// 70 100
+// 출력
+// {'Yujin': 70, 'Hyewon': 100}
+
+let name = prompt("학생 이름을 입력해주세요").split(" ")
+let score = prompt("점수를 입력해주세요").split(" ")
+let object = {}
+for(let i=0;i<name.length;i++){
+  object[name[i]] = Number(score[i])
+}
+console.log(object)
