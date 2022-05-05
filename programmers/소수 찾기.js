36050d2737f2df5891898445e6ae9d737b137f49
Jaenk <jaehy0429@gmail.com>
2022-05-05 23:14:21 +0900

    algorithm 06

--- programmers/소수 찾기.js
+++ programmers/소수 찾기.js
@@ -1,19 +1,37 @@
 // sol1 - 효율성 이슈
+// function solution(n) {
+//   let answer = 0;
+//   if (n >= 2) {
+//     answer = 1;
+//   }
+//   for (let i = 3; i <= n; i++) {
+//     let temp = true;
+//     for (let j = 2; j < i; j++) {
+//       if (i % j === 0) {
+//         temp = false;
+//       }
+//     }
+//     if (temp) {
+//       answer++;
+//     }
+//   }
+//   return answer;
+// }
+
+// sol2
 function solution(n) {
-  let answer = 0;
-  if (n >= 2) {
-    answer = 1;
-  }
-  for (let i = 3; i <= n; i++) {
-    let temp = true;
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        temp = false;
+  let arr = Array(n + 1).fill(1);
+  for (let i = 0; i <= n; i++) {
+    if (i <= 1) {
+      arr[i] = 0;
+    } else {
+      if (arr[i]) {
+        for (let j = 2; j * i <= n + 1; j++) {
+          arr[i * j] = 0;
+        }
       }
     }
-    if (temp) {
-      answer++;
-    }
   }
-  return answer;
+  console.log(arr)
+  return arr.reduce((a,b)=>{return a+b})
 }
