1a5c6745c1bbe90f4b2f652748121919c8133e50
Jaenk <jaehy0429@gmail.com>
2022-05-30 23:46:20 +0900

    algorithm 20

--- programmers/다리를_지나는_트럭.js
+++ programmers/다리를_지나는_트럭.js
@@ -1,24 +1,25 @@
 function solution(bridge_length, weight, truck_weights) {
-    let bridge = Array.from({ length: bridge_length }, () => 0);
-  
-    let time = 0;
-  
-    while (bridge.length) {
-      bridge.shift();
-      time += 1;
-  
-      if (truck_weights.length) {
-        let sum = bridge.reduce(function add(sum, curValue) {
-          return sum + curValue;
-        });
-  
-        if (sum + truck_weights[0] <= weight) {
-          bridge.push(truck_weights.shift());
-        } else {
-          bridge.push(0);
-        }
+  let bridge = Array.from({ length: bridge_length }, () => 0);
+
+  let time = 0;
+
+  while (bridge.length) {
+    bridge.shift();
+    time += 1;
+
+    if (truck_weights.length) {
+      let sum = 0;
+      for (let i = 0; i < bridge.length; i++) {
+        sum += bridge[i];
+      }
+
+      if (sum + truck_weights[0] <= weight) {
+        bridge.push(truck_weights.shift());
+      } else {
+        bridge.push(0);
       }
     }
-  
-    return time;
   }
+
+  return time;
+}
